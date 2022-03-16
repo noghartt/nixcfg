0b3a963a34004bb138ece43c4c8f2e74508189ff
Guilherme Ananias <iamguilhermeananias@gmail.com>
2022-03-16 03:40:50 -0300

    write lib.mkSystem function

---
+++ lib/default.nix
@@ -0,0 +1,20 @@
+{ inputs, overlays }:
+
+let
+  inherit (inputs.nixpkgs) lib;
+in
+{
+  mkSystem = { hostname, system ? builtins.currentSystem, users ? [  ] }:
+    lib.nixosSystem {
+      modules = [
+        ../systems/${hostname}
+        {
+          networking.hostName = hostname;
+          nixpkgs = {
+            inherit overlays;
+            config.allowUnfree = true;
+          };
+        }
+      ] ++ lib.forEach users (user: ../users/${user}/system);
+    };
+}
