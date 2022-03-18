3ef77ee819d47a30e5259f1fcd4f6ce5ac4fb767
Guilherme Ananias <iamguilhermeananias@gmail.com>
2022-03-18 03:43:28 -0300

    add postDeviceCommands

--- systems/thinkpad/hardware-configuration.nix
+++ systems/thinkpad/hardware-configuration.nix
@@ -1,19 +1,36 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 
 let
   btrfsDevice = "/dev/disk/by-label/root";
 in
 {
   imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
+    "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
   boot.initrd = {
     availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
     kernelModules = [ "kvm-amd" ];
+    supportedFilesystems = [ "btrfs" ];
 
-    luks.devices."lvm".device = "/dev/disk/by-label/root";
+    postDeviceCommands = ''
+      mkdir -p /mnt
+      mount -o subvol=root ${btrfsDevice} /mnt
+
+      echo "Cleaning subvolume"
+      btrfs subvolume list -o /mnt/root | cut -f9 -d ' ' |
+      while read subvolume; do
+        btrfs subvolume delete "/mnt/$subvolume"
+      done && btrfs subvolume delete /mnt/root
+
+      echo "Restoring blank subvolume"
+      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+      umount /mnt
+    '';
   };
+
+  boot.initrd.luks.devices."lvm".device = btrfsDevice;
 
   fileSystems = {
     "/boot" = {
@@ -33,4 +50,7 @@ in
       options = [ "subvol=home" ];
     };
   };
+
+  nix.settings.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
