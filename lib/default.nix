aa2024378f7c54dcee506d78b5e7a2c3f5a2dab9
Guilherme Ananias <iamguilhermeananias@gmail.com>
2022-03-20 04:16:02 -0300

    add a comment on mkSystem talking about impure flag of system arg

--- lib/default.nix
+++ lib/default.nix
@@ -4,6 +4,13 @@ let
   inherit (inputs.nixpkgs) lib;
 in
 {
+  # The `builtins.currentSystem` here is a mutable value, so you need to add
+  # `--impure` flag in some cases.
+  #
+  # E.g.: nix flake check --impure
+  #
+  # TODO: This should be an impure value? Or we can add a default string value?
+  # `"x86_64-linux"` for example.
   mkSystem = { hostname, system ? builtins.currentSystem, users ? [  ] }:
     lib.nixosSystem {
       inherit system;
