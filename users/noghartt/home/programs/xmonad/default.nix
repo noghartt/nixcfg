faa9a79f91bf394e3dc14e8d9f14101db2c33511
Guilherme Ananias <iamguilhermeananias@gmail.com>
2022-04-07 21:15:52 -0300

    bump xmonad packages to v0.17

--- users/noghartt/home/programs/xmonad/default.nix
+++ users/noghartt/home/programs/xmonad/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs }:
 
 {
   xsession = {
@@ -7,7 +7,16 @@
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
+
       config = ./xmonad.hs;
+
+      extraPackages = haskellPackages: with haskellPackages; [
+        xmonad_0_17_0
+        xmonad-contrib_0_17_0
+        xmonad-extras_0_17_0
+      ];
     };
   };
+
+  home.packages = with pkgs; [ dmenu ];
 }
