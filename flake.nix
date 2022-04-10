de6f651e0b9805b2f0e8864bfff84386f7469884
Guilherme Ananias <hi@noghartt.dev>
2022-04-10 13:20:24 -0300

    add emacs-overlay and doom-emacs as flake inputs

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS configuration";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nur.url = "github:nix-community/NUR";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
@@ -17,14 +17,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    doom-emacs = {
+      url = "github:nix-community/nix-doom-emacs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.emacs-overlay.follows = "emacs-overlay";
+    };
   };
 
-  outputs = { self, ... }@inputs:
+  outputs = { ... }@inputs:
     let
       system = builtins.currentSystem or "x86_64-linux";
 
       overlays = with inputs; [
         nur.overlay
+        emacs-overlay.overlay
       ];
 
       # TODO: Is necessary this `config.allowUnfree` here? Or just the
@@ -34,9 +43,13 @@
         config = { allowUnfree = true; };
       };
 
-      lib = pkgs.callPackages ./lib { inherit inputs; };
+      lib = pkgs.callPackage ./lib { inherit inputs overlays; };
     in
       {
+        inherit overlays;
+
+        package = pkgs;
+
         nixosConfigurations = {
           thinkpad = lib.mkHost {
             hostname = "thinkpad";
