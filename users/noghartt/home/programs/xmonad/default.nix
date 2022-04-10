b4249e97cf2343a3e6513738ae05506a27e560df
Guilherme Ananias <hi@noghartt.dev>
2022-04-10 13:22:10 -0300

    remove 0_17_0 suffix from xmonad extraPackages

--- users/noghartt/home/programs/xmonad/default.nix
+++ users/noghartt/home/programs/xmonad/default.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, ... }:
 
 {
   xsession = {
@@ -11,9 +11,9 @@
       config = ./xmonad.hs;
 
       extraPackages = haskellPackages: with haskellPackages; [
-        xmonad_0_17_0
-        xmonad-contrib_0_17_0
-        xmonad-extras_0_17_0
+        xmonad
+        xmonad-contrib
+        xmonad-extras
       ];
     };
   };
