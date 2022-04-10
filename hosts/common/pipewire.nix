59a59818638d7b8709bc404b01f2e3046c17ed43
Guilherme Ananias <hi@noghartt.dev>
2022-04-10 17:24:05 -0300

    move pipewire configuration to common/pipewire.nix

---
+++ hosts/common/pipewire.nix
@@ -0,0 +1,16 @@
+{
+  sound.enable = true;
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+  };
+}
