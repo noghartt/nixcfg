924d7249f7f941fd1169164a125d87d54be92910
Guilherme Ananias <hi@noghartt.dev>
2022-04-11 22:53:19 -0300

    add some useful packages to noghartt e.g.: google-chrome, steam, etc

--- users/noghartt/home/home.nix
+++ users/noghartt/home/home.nix
@@ -18,6 +18,17 @@
     pavucontrol
     discord
     neofetch
+    google-chrome
+    # NOTE: We need to add `gh` as a "simple" package because it's broken.
+    #       If we try to add as `program.gh` and do the auth flow, it will
+    #       log a error saying that the `config.yml` file is a readonly file.
+    #       There's a PR fixing it, see here: https://github.com/cli/cli/pull/5378
+    #
+    # TODO: Maybe we could write an overlay pointing the fix on this PR.
+    gh
+    nyxt
+    steam
+    xfce.thunar
   ];
 
   home.file."homecfg" = {
