f331b96437a1db1b4b7a9968c376e88886753999
Guilherme Ananias <hi@noghartt.dev>
2022-05-05 12:10:51 -0300

    add gnome-keyring on ssh file

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -1,7 +1,8 @@
 {
   services.openssh.enable = true;
 
-  programs.ssh = {
-    startAgent = true;
-  };
+  services.gnome3.gnome-keyring.enable = true;
+  security.pam.services.lightdm.enableGnomeKeyring = true;
+
+  programs.ssh.startAgent = true;
 }
