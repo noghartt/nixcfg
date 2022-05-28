6d1306b15d4f6b945b6eb3e065e988adbf81e841
Guilherme Ananias <hi@noghartt.dev>
2022-05-28 18:13:47 -0300

    move home-manager to a nixos module

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,4 @@
-{ inputs, overlays ? [  ], pkgs, lib }:
+{ inputs, overlays ? [  ] }:
 
 let
   inherit (inputs) nixpkgs home-manager;
@@ -7,41 +7,55 @@ in
   mkHost = { hostname, system ? "x86_64-linux", users ? [  ] }:
     nixpkgs.lib.nixosSystem {
       inherit system;
+
       specialArgs = {
         inherit inputs system hostname;
       };
+
       modules = [
+        home-manager.nixosModule
+
         ../hosts/${hostname}
         {
           networking.hostName = hostname;
-          nixpkgs = {
-            inherit overlays;
-            config.allowUnfree = true;
-          };
-        }
-      ] ++ lib.forEach users (user: ../users/${user});
-    };
-
-  mkHome = { username, system, hostname }:
-    home-manager.lib.homeManagerConfiguration {
-      inherit username system;
-
-      extraSpecialArgs = {
-        inherit system username hostname inputs;
-      };
-
-      homeDirectory = "/home/${username}";
-      configuration = ../users/${username}/home/home.nix;
-      stateVersion = "21.11";
-      extraModules = [
-        {
+         
           nixpkgs = {
             inherit overlays;
             config.allowUnfree = true;
           };
 
-          programs.home-manager.enable = true;
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+
+            sharedModules = [
+              inputs.doom-emacs.hmModule
+            ];
+          };
         }
-      ];
+      ] ++ nixpkgs.lib.forEach users (user: ../users/${user});
     };
+
+  # mkHome = { username, system, hostname }:
+  #   home-manager.lib.homeManagerConfiguration {
+  #     inherit username system;
+
+  #     extraSpecialArgs = {
+  #       inherit system username hostname inputs;
+  #     };
+
+  #     homeDirectory = "/home/${username}";
+  #     configuration = ../users/${username}/home/home.nix;
+  #     stateVersion = "21.11";
+  #     extraModules = [
+  #       {
+  #         nixpkgs = {
+  #           inherit overlays;
+  #           config.allowUnfree = true;
+  #         };
+
+  #         programs.home-manager.enable = true;
+  #       }
+  #     ];
+  #   };
 }
