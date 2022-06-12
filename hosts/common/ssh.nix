f3e68fa503bb87fb951007447e8e6865454599de
Guilherme Ananias <hi@noghartt.dev>
2022-06-12 15:24:01 -0300

    replace gnome for gnome3 in common/ssh config

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -1,7 +1,7 @@
 {
   services.openssh.enable = true;
 
-  services.gnome3.gnome-keyring.enable = true;
+  services.gnome.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
 
   programs.ssh.startAgent = true;
