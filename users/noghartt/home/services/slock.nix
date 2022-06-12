88e4ab674d940648cdb38b79dcf565399d836cbb
Guilherme Ananias <hi@noghartt.dev>
2022-06-12 15:29:20 -0300

    add slock as home-manager package

---
+++ users/noghartt/home/services/slock.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  services.screen-locker = {
+    enable = true;
+    lockCmd = "${pkgs.slock}/bin/slock";
+
+    xautolock = {
+      enable = true;
+    };
+  };
+
+  systemd.services.lock-before-sleeping = {
+    restartIfChanged = false;
+
+    unitConfig = {
+      Description = "Helper service to bind locker to sleep.target";
+    };
+
+    serviceConfig = {
+      ExecStart = "${pkgs.slock}/bin/slock";
+      Type = "simple";
+    };
+
+    before = [ "pre-sleep.service" ];
+    wantedBy = [ "pre-sleep.service" ];
+
+    environment = {
+      DISPLAY = ":0";
+      XAUTHORITY = "/home/noghartt/.Xauthority";
+    };
+  };
+}
