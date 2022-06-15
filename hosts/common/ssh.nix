2c34092d08b90a2105ded78845307fce5054f402
Guilherme Ananias <hi@noghartt.dev>
2022-06-15 10:19:50 -0300

    add AddKeysToAgent as yes on SSH config

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -1,8 +1,15 @@
 {
   services.openssh.enable = true;
 
+  programs.ssh = {
+    startAgent = true;
+
+    extraConfig = ''
+      AddKeysToAgent yes
+    '';
+  };
+
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
 
-  programs.ssh.startAgent = true;
 }
