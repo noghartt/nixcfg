efe558383c3ba8bb2892e3626c7f0ee6f3df581d
Guilherme Ananias <iamguilhermeananias@gmail.com>
2024-12-25 12:03:19 -0300

    add cronjob each 1h to commit ledger account

--- hosts/mbp/launchd.nix
+++ hosts/mbp/launchd.nix
@@ -1,11 +1,14 @@
 _:
 
+let
+  beancount_ledger_folder = "$HOME/ledger";
+in
 {
   launchd = {
     user = {
       agents = {
         beancount-fava = {
-          command = "/opt/homebrew/bin/fava $HOME/ledger/*.bean";
+          command = "/opt/homebrew/bin/fava ${beancount_ledger_folder}/*.bean";
           serviceConfig = {
             UserName = "noghartt";
             KeepAlive = true;
@@ -13,6 +16,26 @@ _:
             StandardOutPath = "/tmp/fava_beancount.out.log";
             StandardErrorPath = "/tmp/fava_beancount.err.log";
           };
+        };
+
+        beancount-commit = {
+          serviceConfig = {
+            UserName = "noghartt";
+            KeepAlive = true;
+            RunAtLoad = true;
+            StandardOutPath = "/tmp/beancount-commit.out.log";
+            StandardErrorPath = "/tmp/beancount-commit.err.log";
+            StartInterval = 3600;
+          };
+
+          command = ''
+            cd ${beancount_ledger_folder} && \
+            if [[ -n $(git status -s) ]]; then \
+                git add . && \
+                git commit -m "*" && \
+                git push; \
+            fi
+          '';
         };
       };
     };
