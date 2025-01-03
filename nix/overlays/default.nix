7563a9625fe1a14a1c490c7bad56a21db8781c6b
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-02 21:37:35 -0300

    override python packages for all extensions

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,22 +1,19 @@
 final: prev:
 
 let
-  pythonOverrides = {
-    packageOverrides = self: super: {
-      bean-price = prev.callPackage ./bean-price {
-        inherit (super) buildPythonPackage isPy3k;
-      };
-
-      beancount = super.beancount.overrideAttrs (oldAttrs: {
-        postInstall = ''
-          ${oldAttrs.postInstall or ""}
-          rm $out/bin/bean-price
-        '';
-      });
+  pythonPackageExtensionsOverrides = self: super: {
+    bean-price = prev.callPackage ./bean-price {
+      inherit (super) buildPythonPackage isPy3k;
     };
+
+    beancount = super.beancount.overrideAttrs (oldAttrs: {
+      postInstall = ''
+        ${oldAttrs.postInstall or ""}
+        rm $out/bin/bean-price
+      '';
+    });
   };
 in
 {
-  python39 = prev.python39.override pythonOverrides;
-  python312 = prev.python312.override pythonOverrides;
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [pythonPackageExtensionsOverrides];
 }
