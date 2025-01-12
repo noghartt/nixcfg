d550fbba80988a0d867ad33a3d11cf46e685af55
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-11 23:41:14 -0300

    add initial setup for lazy.nvim and telescope.lua plugin

---
+++ neovim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end
+      },
+    },
+  },
+}
