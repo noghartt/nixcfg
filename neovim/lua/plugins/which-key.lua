bb6b514e818fb1f9e44df3e47cab604fef507ea5
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-23 07:42:15 -0300

    add new keys for workspace and alpha

--- neovim/lua/plugins/which-key.lua
+++ neovim/lua/plugins/which-key.lua
@@ -1,16 +1,25 @@
 return {
   {
-    'folke/which-key.nvim',
+    "folke/which-key.nvim",
     event = "VimEnter",
 
     opts = {
       delay = 0,
 
       spec = {
-        { "<leader>s", group = '[S]earch' },
+        { "<leader>a", "<cmd>Alpha<CR>", desc = "Go to [A]lpha" },
+        { "<leader>s", group = "[S]earch" },
+
+        { "<leader>f", group = "[F]ile" },
+        { "<leader>fs", "<cmd>w<CR>", desc = "[F]ile [S]ave" },
 
         { "<leader>o", group = "[O]pen" },
-        { "<leader>oe", "<cmd>Oil<CR>", desc = "Explore" },
+        { "<leader>oe", "<cmd>Oil<CR>", desc = "[O]pen [E]xplorer" },
+        {
+          "<leader>ow",
+          "<cmd>Telescope workspaces<CR>",
+          desc = "[O]pen [W]orkspaces",
+        },
       },
     },
   },
