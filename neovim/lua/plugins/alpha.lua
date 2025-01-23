bbca61a09d53be2328bb92f62cdde66f9d1580b8
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-22 22:25:11 -0300

    add alpha on plugins

---
+++ neovim/lua/plugins/alpha.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local startify = require "alpha.themes.startify"
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(startify.config)
+    end,
+  },
+}
