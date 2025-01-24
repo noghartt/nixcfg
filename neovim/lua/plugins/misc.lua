e90ab512112479e1ef63aa4364964d62e7dd8d12
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-23 22:05:43 -0300

    add support to direnv.nvim

--- neovim/lua/plugins/misc.lua
+++ neovim/lua/plugins/misc.lua
@@ -1,23 +1,24 @@
 return {
+  { "direnv/direnv.vim" },
+
   {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function ()
-      require('nvim-autopairs').setup {}
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("nvim-autopairs").setup {}
       -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+      local cmp = require "cmp"
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
-    main = 'ibl',
+    main = "ibl",
     opts = {},
   },
 }
-
