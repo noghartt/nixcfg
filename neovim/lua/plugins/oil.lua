2e5acc9c3dc1ebdc4409effc313d0276c2c68eec
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-01-30 23:57:11 -0300

    simplify oil settings

--- neovim/lua/plugins/oil.lua
+++ neovim/lua/plugins/oil.lua
@@ -1,23 +1,20 @@
 return {
   {
     "stevearc/oil.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icons",
+        "permissions",
+        "size",
+        "mtime",
+      },
+      delete_to_trash = true,
+      cleanup_delay_ms = 1000,
+      use_default_keymaps = true,
     },
-    config = function ()
-      require("oil").setup({
-        default_file_explorer = true,
-        columns = {
-          "icons",
-          "permissions",
-          "size",
-          "mtime"
-        },
-        delete_to_trash = true,
-        cleanup_delay_ms = 1000,
-        use_default_keymaps = true,
-      })
-    end
-  }
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
