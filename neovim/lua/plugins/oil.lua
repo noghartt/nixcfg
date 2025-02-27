7a3a5ae1be821f244499e119d07b3ff2b0827355
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-02-26 22:02:36 -0300

    show hidden files on oil

--- neovim/lua/plugins/oil.lua
+++ neovim/lua/plugins/oil.lua
@@ -12,6 +12,9 @@ return {
       delete_to_trash = true,
       cleanup_delay_ms = 1000,
       use_default_keymaps = true,
+      view_options = {
+        show_hidden = true,
+      },
     },
     dependencies = {
       "nvim-tree/nvim-web-devicons",
