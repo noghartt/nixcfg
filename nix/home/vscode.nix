fcf7dffadc596f87237d34a0c11db2ee0d53c179
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-04-08 11:26:49 -0300

    move vscode config to profile

--- nix/home/vscode.nix
+++ nix/home/vscode.nix
@@ -4,32 +4,34 @@
   programs.vscode = {
     enable = true;
 
-    extensions = with pkgs.vscode-extensions; [
-      editorconfig.editorconfig
-      usernamehw.errorlens
-      bbenoist.nix
-      pkief.material-icon-theme
-      astro-build.astro-vscode
-      eamodio.gitlens
-      dbaeumer.vscode-eslint
-      esbenp.prettier-vscode
-      vscodevim.vim
-      ziglang.vscode-zig
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        editorconfig.editorconfig
+        usernamehw.errorlens
+        bbenoist.nix
+        pkief.material-icon-theme
+        astro-build.astro-vscode
+        eamodio.gitlens
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+        vscodevim.vim
+        ziglang.vscode-zig
+      ];
 
-    userSettings = {
-      "workbench.iconTheme" = "material-icon-theme";
-      "workbench.startupEditor" = "none";
+      userSettings = {
+        "workbench.iconTheme" = "material-icon-theme";
+        "workbench.startupEditor" = "none";
 
-      "editor.tabSize" = 2;
-      "editor.codeActionsOnSave" = {
-        "source.fixAll" = "explicit";
+        "editor.tabSize" = 2;
+        "editor.codeActionsOnSave" = {
+          "source.fixAll" = "explicit";
+        };
+
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+
+        "zig.zls.enabled" = "on";
       };
-
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-
-      "zig.zls.enabled" = "on";
     };
   };
 }
