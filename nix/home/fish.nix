68902673abfc7f432cff453a960fde9d3b3c9b2c
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-04-19 17:50:34 -0300

    automatically start tmux on fish startup

--- nix/home/fish.nix
+++ nix/home/fish.nix
@@ -16,6 +16,11 @@ _:
       fish_add_path -amP /opt/local/bin
       fish_add_path -m /run/current-system/sw/bin
       fish_add_path -m $HOME/.nix-profile/bin
+
+      if status is-interactive
+      and not set -q TMUX
+        exec tmux
+      end
     '';
 
     shellAliases = {
--- nix/home/tmux.nix
+++ nix/home/tmux.nix
@@ -4,10 +4,13 @@
   programs.tmux = {
     enable = true;
 
-    shell = "${pkgs.zsh}/bin/zsh";
+    shell = "${pkgs.fish}/bin/fish";
     mouse = true;
     terminal = "screen-256color";
     historyLimit = 50000;
+    keyMode = "vi";
+
+    tmuxinator.enable = true;
 
     plugins = with pkgs; [
       tmuxPlugins.better-mouse-mode
