551f9c7b4b84b2a4dc7087bac8042cedb8662ae3
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-04-19 22:58:39 -0300

    add bindings on tmux to window navigate

--- nix/home/tmux.nix
+++ nix/home/tmux.nix
@@ -15,5 +15,12 @@
     plugins = with pkgs; [
       tmuxPlugins.better-mouse-mode
     ];
+
+    extraConfig = ''
+      bind -n C-h select-pane -L
+      bind -n C-j select-pane -D
+      bind -n C-k select-pane -U
+      bind -n C-l select-pane -R
+    '';
   };
 }
