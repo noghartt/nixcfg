c07d8797867aec9a06df31e171e9f778b2d4419c
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-05-06 23:28:06 -0300

    bump rustaceanvim to v6

--- neovim/lua/plugins/langs.lua
+++ neovim/lua/plugins/langs.lua
@@ -1,22 +1,8 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
+    version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy
     ft = "rust",
-    config = function()
-      local mason_registry = require "mason-registry"
-      local codelldb = mason_registry.get_package "codelldb"
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-      local cfg = require "rustaceanvim.config"
-
-      vim.g.rustaceanvim = {
-        dap = {
-          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-      }
-    end,
   },
 }
