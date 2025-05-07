1a239631be14fe092ac15a8afa6681b663e1b086
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-05-07 12:06:34 -0300

    remove gap/padding for tiles/accordion layouts on aerospace

--- nix/home/aerospace.nix
+++ nix/home/aerospace.nix
@@ -8,6 +8,17 @@ _:
       start-at-login = true;
       default-root-container-layout = "tiles";
 
+      accordion-padding = 0;
+
+      gaps = {
+        inner.horizontal = 0;
+        inner.vertical = 0;
+        outer.left = 0;
+        outer.right = 0;
+        outer.top = 0;
+        outer.bottom = 0;
+      };
+
       mode.main.binding = {
         ctrl-alt-enter = "exec-and-forget open -na /Applications/Ghostty.app";
         alt-slash = "layout tiles horizontal vertical";
