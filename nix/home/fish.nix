bf7ee65634b95f73af73269ffd433af3f9cb8f7b
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-06-20 23:01:12 -0300

    add cargo bin folder to fish PATH

--- nix/home/fish.nix
+++ nix/home/fish.nix
@@ -16,6 +16,7 @@ _:
       fish_add_path -amP /opt/local/bin
       fish_add_path -m /run/current-system/sw/bin
       fish_add_path -m $HOME/.nix-profile/bin
+      fish_add_path -m $HOME/.cargo/bin
 
       if status is-interactive
       and not set -q TMUX
